8157235c605ef061a29d58069cd86d20ff478a04
YY Ventures <daisydewan@daisys-iMac.local>
2023-12-11 12:19:51 +0600

    fixed eslint error

--- src/app/health-force/page.tsx
+++ src/app/health-force/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable operator-linebreak */
 import React from 'react';
 
 import { useFetch } from '@/shared/hook';
--- src/app/healthcare/[healthcareService]/page.tsx
+++ src/app/healthcare/[healthcareService]/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 import React from 'react';
 
 import { useFetch } from '@/shared/hook';

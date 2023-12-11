66d22fa1ae0d66d547d81411bca093269c72cf29
YY Ventures <daisydewan@daisys-iMac.local>
2023-12-11 12:50:56 +0600

    removed eslint rules

--- src/app/photo-gallery/page.tsx
+++ src/app/photo-gallery/page.tsx
@@ -4,7 +4,7 @@ import { useFetch } from '@/shared/hook';
 import Album from '@/components/PhotoGallery/Album/Album';
 
 const PhotoGallery = async () => {
-  //Fetching album data
+
   const photoAlbums = await useFetch({ url: '/photo-albums' });
 
   return (
